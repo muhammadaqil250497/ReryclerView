8b9eff80f5b532e8642f193e55f3216e305bab87
BEST LAPTOP <muhammadaqil250497@gmail.com>
2023-05-14 12:12:00 +0700

    Recyclerview APP

---
+++ app/src/main/java/com/muhammadaqil/recyclerview/MainActivity.kt
@@ -0,0 +1,57 @@
+package com.muhammadaqil.recyclerview
+
+import android.app.Dialog
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Window
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import com.muhammadaqil.recyclerview.adapter.AdapterTeamBola
+import com.muhammadaqil.recyclerview.databinding.ActivityMainBinding
+import com.muhammadaqil.recyclerview.model.Pemain
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val listPemain = ArrayList<Pemain>()
+        listPemain.add(Pemain("Thibaut Courtois",R.drawable.courtois,"Penjaga Gawang", "2.00 m", "Bree (Belgia)", "11 Mei 1992"))
+        listPemain.add(Pemain("Karim Benzema",R.drawable.courtois,"Penyerang", "1,85 m", "Lyon (Perancis)", "19 Desember 1987"))
+        listPemain.add(Pemain("Marcelo Vieira da Silva",R.drawable.courtois,"Belakang", "1,74 m", "Rio de Janeiro (Brasil)", "12 Mei 1988"))
+        listPemain.add(Pemain("Sergio Ramos García",R.drawable.courtois,"Belakang", "1,84 m", "Camas (Sevilla)", "30 Maret 1986"))
+        listPemain.add(Pemain("Zinedine Yazid Zidane",R.drawable.courtois,"Pelatih", "1,85 m", "Marseille (Prancis)", "23 Juni 1972"))
+
+        binding.list.adapter = AdapterTeamBola(this,listPemain,object : AdapterTeamBola.OnClickListener {
+            override fun detailData(item: Pemain?) {
+                Dialog(this@MainActivity).apply {
+                    requestWindowFeature(Window.FEATURE_NO_TITLE)
+                    setCancelable(true)
+                    setContentView(R.layout.detail_data_pemain)
+
+                    val image = this.findViewById<ImageView>(R.id.image_pemain)
+                    val nama = this.findViewById<TextView>(R.id.textNamaPemain)
+                    val posisi = this.findViewById<TextView>(R.id.textPosisi)
+                    val tinggi = this.findViewById<TextView>(R.id.textTinggi)
+                    val tempatlahir = this.findViewById<TextView>(R.id.textTempatlahir)
+                    val tgllahir = this.findViewById<TextView>(R.id.textTanggallahir)
+                    val btn = this.findViewById<Button>(R.id.btnClose)
+
+                    image.setImageResource(item?.foto ?:0)
+                    nama.text = "${item?.nama}"
+                    posisi.text ="${item?.posisi}"
+                    tinggi.text = "${item?.tinggi}"
+                    tempatlahir.text = "${item?.tempatlahir}"
+                    tgllahir.text = "${item?.tgllahir}"
+
+                    btn.setOnClickListener {
+                        this.dismiss()
+                    }
+                }.show()
+            }
+        })
+    }
+}
